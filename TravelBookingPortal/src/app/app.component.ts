ef5c88f6e8a7f8d414ef82f25f816264517ed1a4
marwan abed <marwanabed453@gmail.com>
2025-04-08 06:57:25 +0200

    login with spinner

--- TravelBookingPortal/src/app/app.component.ts
+++ TravelBookingPortal/src/app/app.component.ts
@@ -10,7 +10,9 @@ import { GoTopComponent } from './Components/go-top/go-top.component';
 import { ContactComponent } from './Components/contact/contact.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { ToursearchComponent } from './Components/toursearch/toursearch.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Import library module
+import { NgxSpinnerModule } from 'ngx-spinner';
 @Component({
   selector: 'app-root',
   imports: [
@@ -23,6 +25,8 @@ import { ToursearchComponent } from './Components/toursearch/toursearch.componen
     GoTopComponent,
     ContactComponent,
     FooterComponent,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
     ToursearchComponent,
   ],
   templateUrl: './app.component.html',
