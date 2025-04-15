682824b51816f9adae978f691ae983ccf2643a1d
rehab-megahed123 <rehabmegahed241@gmail.com>
2025-04-15 19:08:55 +0200

    Edit NavBar

--- TravelBookingPortal/src/app/Pages/itinerary/itinerary.component.ts
+++ TravelBookingPortal/src/app/Pages/itinerary/itinerary.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TripPlannerComponent } from '../../Components/trip/trip-planner/trip-planner/trip-planner.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../../Components/footer/footer.component";
+import { NavBarComponent } from "../../Components/Shared/navbar/nav-bar.component";
 
 @Component({
   selector: 'app-itinerary',
-  imports: [TripPlannerComponent ,CommonModule],
+  imports: [TripPlannerComponent, CommonModule, FooterComponent, NavBarComponent],
   templateUrl: './itinerary.component.html',
   styleUrl: './itinerary.component.css'
 })
