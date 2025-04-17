b1a4a2bd9db4f555ee7698fe2f099ce664b0ff36
Menna-Sameh <mennasameh504@gmail.com>
2025-04-17 23:27:31 +0200

    Itinerary Add , Edit , Forms

--- TravelBookingPortal/src/app/core/services/trip.service.ts
+++ TravelBookingPortal/src/app/core/services/trip.service.ts
@@ -16,20 +16,19 @@ export class TripService {
     return this._httpClientService.get<ITrip[]>(`${environment.baseUrl}/api/Itinerary/get-all`);
   }
 
-  // Get a specific trip
-  getTripById(itineraryId: number): Observable<ITrip> {
-    return this._httpClientService.get<ITrip>(`${environment.baseUrl}/api/Itinerary/get/${itineraryId}`);
+  getUserTrips(): Observable<ITrip[]> {
+    return this._httpClientService.get<ITrip[]>(`${environment.baseUrl}/api/Itinerary/user`);
   }
 
   // Add a new trip
   addTrip(newTrip: ITrip): Observable<ITrip> {
-    console.log('Sending new trip to API:', newTrip); 
+    console.log('Sending new trip to API:', newTrip);
     return this._httpClientService.post<ITrip>(`${environment.baseUrl}/api/Itinerary/add`, newTrip);
   }
 
   // Edit a trip
-  editTrip(userId: string, itineraryId: number, updatedTrip: ITrip): Observable<ITrip> {
-    return this._httpClientService.put<ITrip>(`${environment.baseUrl}/api/Itinerary/edit/${userId}/${itineraryId}`, updatedTrip);
+  editTrip(updatedTrip: ITrip): Observable<any> {
+    return this._httpClientService.put(`${environment.baseUrl}/api/Itinerary/edit`, updatedTrip);
   }
 
   // Delete a trip
